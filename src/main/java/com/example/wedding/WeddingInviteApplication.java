8daced4f0083dbff685faa6d0d014970116bb099
amirhanordobaev <amirhanordobaev@kstu.kg>
2025-11-06 20:53:03 +0600

    Fix main class name inside WeddingInviteApplication.java to match filename

--- src/main/java/com/example/wedding/WeddingInviteApplication.java
+++ src/main/java/com/example/wedding/WeddingInviteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeddingApplication {
+public class WeddingInviteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeddingApplication.class, args);
+        SpringApplication.run(WeddingInviteApplication.class, args);
     }
 
 }
